f8551222ad5ecaee8642f92c5b2f21796c03dc92
matejcik <ja@matejcik.cz>
2019-03-29 16:54:19 +0100

    neatly arranged test cases

--- src/main/scala/newcase/TestScenario.scala
+++ src/main/scala/newcase/TestScenario.scala
@@ -11,6 +11,11 @@ import scala.util.control.Breaks._
 object TestScenario {
   val TEST_ROUNDS = 15
 
+  val WORKER_RANGE = 5 to 42
+  val ROOM_RANGE = 3 to 15
+  val PROJECT_RANGE = 3 to 15
+  val CAPACITY_RANGE = 1 to 25
+
   val logWriter = new PrintWriter(new File("lunch.log"))
   val perfLogWriter = new PrintWriter(new File("lunch-perf.log"))
 
@@ -37,17 +42,25 @@ object TestScenario {
     val max = formatMs(measurements.max)
     val avg = formatMs(measurements.sum / TEST_ROUNDS)
     val med = formatMs(measurementsSorted(TEST_ROUNDS / 2))
-    log(s"Scenario ${spec.toPerfLine} solved in avg $avg ms (min: $min, max: $max, med: $med), utility $utility")
+    log(s"Scenario ${spec} solved in avg $avg ms (min: $min, max: $max, med: $med), utility $utility")
   }
 
   def solveScenario(spec: ScenarioSpec) = {
     val model = ModelGenerator.modelFromSpec(spec)
 
+    //val fullUtility = model.rooms.map(r => r.capacity * r.capacity).sum
+
     val start = System.nanoTime()
     model.problem.init()
-    //val init = System.nanoTime()
+    val init = System.nanoTime()
     //println(s"   init ran in ${(init - start) / 1000000.0} ms")
-    while (model.problem.solve()) {}
+    //var foundMaxUtility = false
+    while (model.problem.solve()) {
+//      if (model.problem.instance.solutionUtility >= fullUtility) {
+//        val solutionTime = System.nanoTime()
+//        println(s"   found full solution after ${formatMs(solutionTime - init)} ms")
+//      }
+    }
     if (model.problem.exists) {
       model.problem.commit()
       //      for (action <- model.problem.actions) {
@@ -55,6 +68,7 @@ object TestScenario {
       //      }
     }
     val end = System.nanoTime()
+//    println(s"   full set took ${formatMs(end - start)} ms")
 
     (model.problem.exists(), end - start, model.problem.instance.solutionUtility)
   }
@@ -71,18 +85,43 @@ object TestScenario {
   }
 
 
+  def measure_workerCount: Unit = {
+    for (workerCount <- WORKER_RANGE) {
+      val specRegular = ScenarioSpec(3, 3, 10, false, workerCount, ProjectAssignment.ROUND_ROBIN)
+      measureScenario(specRegular)
+      val specRandom = ScenarioSpec(3, 3, 20, true, workerCount, ProjectAssignment.ROUND_ROBIN)
+      measureScenario(specRandom)
+    }
+  }
+
+  def measure_roomCount: Unit = {
+    for (roomCount <- ROOM_RANGE) {
+      val specRegular = ScenarioSpec(3, roomCount, 10, false, 20, ProjectAssignment.ROUND_ROBIN)
+      measureScenario(specRegular)
+      val specRandom = ScenarioSpec(3, roomCount, 20, true, 20, ProjectAssignment.ROUND_ROBIN)
+      measureScenario(specRandom)
+    }
+  }
+
+  def measure_projectCount: Unit = {
+    for (projectCount <- PROJECT_RANGE) {
+      val specRegular = ScenarioSpec(projectCount, 15, 10, false, 30, ProjectAssignment.ROUND_ROBIN)
+      measureScenario(specRegular)
+      val specRandom = ScenarioSpec(projectCount, 15, 10, false, 30, ProjectAssignment.RANDOM)
+      measureScenario(specRandom)
+    }
+  }
+
+
   def main(args: Array[String]): Unit = {
 
     warmup
 
-    for (workerCount <- 5 to 45) {
-      val spec = ScenarioSpec(3, 3, 10, false, workerCount, ProjectAssignment.ROUND_ROBIN)
-      measureScenario(spec)
-    }
-
-    for (workerCount <- 5 to 45) {
-      val spec = ScenarioSpec(3, 3, 10, true, workerCount, ProjectAssignment.ROUND_ROBIN)
-      measureScenario(spec)
-    }
+    log("===== measuring varying worker counts =====")
+    measure_workerCount
+    log("===== measuring varying room counts =====")
+    measure_roomCount
+    log("===== measuring varying project counts =====")
+    measure_projectCount
   }
 }
