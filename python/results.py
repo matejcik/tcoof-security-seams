ea14d52503240b0f74a5a398e9b9b86be3d75334
matejcik <ja@matejcik.cz>
2019-04-11 16:09:41 +0200

    result formatting code

--- python/results.py
+++ python/results.py
@@ -1,122 +1,172 @@
+import os
+import colorsys
+
 import matplotlib
 
 matplotlib.use("Agg")
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
-import csv
+HERE = os.path.dirname(__file__)
+RESULT_PATH = os.path.abspath(os.path.join(HERE, "..", "results", "2019-04-09"))
+
+COLUMNS = {
+    "projects": np.int32,
+    "lunch_n": np.int32,
+    "lunch_cap": np.int32,
+    "work_n": np.int32,
+    "work_cap": np.int32,
+    "workers": np.int32,
+    "hungry": np.int32,
+    "preassigned": np.int32,
+    "lunchtime": str,
+    "i": np.int32,
+    "time": str,
+}
+
+NS_TIME = int(30e9)
 
 
-def read_csv(filename, scaling_factor):
-    with open(f"data/{filename}.log") as csv_file:
-        data = []
+def read_csv(label, scaling_factor=1e-6):
+    filename = os.path.join(RESULT_PATH, label + ".log")
+    df = pd.read_csv(
+        filename,
+        header=None,
+        names=COLUMNS.keys(),
+        dtype=COLUMNS,
+        skipinitialspace=True,
+    )
 
-        csv_reader = csv.reader(csv_file)
-        for row in csv_reader:
-            data.append(row)
+    df.lunchtime = df.lunchtime == "true"
+    df["failed"] = df.time == "FAIL"
+    df["nsec"] = df.time.replace("FAIL", NS_TIME).astype(np.int64)
+    df["nsec"] *= scaling_factor
 
-        np_data = np.asarray(data, dtype=np.float32)
-        np_data[:, 5] = np_data[:, 5] * scaling_factor
-
-        return np_data
-
-
-data = np.concatenate((read_csv("0", 1e-6), read_csv("1", 1)))
-
-measure_phases = np.unique(data[:, 0])
-factories_counts = np.unique(data[:, 1])
-workers_per_workplace_counts = np.unique(data[:, 2])
-workers_late_ratios = np.unique(data[:, 3])
-
-# print(measure_phases)
-# print(factories_counts)
-# print(workers_per_workplace_counts)
-# print(workers_late_ratios)
+    return df
 
 
-def set_box_color(bp, color):
-    plt.setp(bp["boxes"], color=color)
-    plt.setp(bp["whiskers"], color=color)
-    plt.setp(bp["caps"], color=color)
-    plt.setp(bp["medians"], color=color)
+def box_graph(datasets, labels, colors, x_ticks):
+    """
+    Datasets is a list of lists of series: the outer list is the number of "lines"
+    (i.e., different measure types that will be shown per each tick). 
+    The inner list has an element for each data point (each x-ticks), and is a series
+    of values from which a single candlebar is drawn.
 
+    `colors` is a list of hex/rgb colors, same length as `datasets`
 
-group_size = len(workers_late_ratios)
-space_size = 1
-step_size = group_size + space_size
-colors = ["#e41a1c", "#377eb8", "#4daf4a", "#984ea3"]
+    `x_ticks` is a list of values on the X axis, same length as elements of `datasets`
+    """
 
-factories_count = np.float32(1)
-
-for measure_phase in measure_phases:
-
-    expected_no_of_data_points = 10000 if measure_phase == 0 else 100
+    def setcolor(bp, color):
+        plt.setp(bp["boxes"], color=color)
+        plt.setp(bp["whiskers"], color=color)
+        plt.setp(bp["caps"], color=color)
+        plt.setp(bp["medians"], color=color)
 
     fig = plt.figure(figsize=(7, 5), dpi=300)
-    ax = fig.add_subplot(1, 1, 1)
+    ax = fig.add_subplot(111)
 
-    for workers_late_ratio_idx, (workers_late_ratio, color) in enumerate(
-        zip(workers_late_ratios, colors)
-    ):
-        boxes = []
+    assert len(datasets) <= len(colors)
+    assert len(datasets) <= len(labels)
+    for dataset in datasets:
+        if len(dataset) < len(x_ticks):
+            dataset.extend([[]] * (len(x_ticks) - len(dataset)))
 
-        for workers_per_workplace_count in workers_per_workplace_counts:
-            data_selection = np.equal(
-                data[:, 0:4],
-                [
-                    measure_phase,
-                    factories_count,
-                    workers_per_workplace_count,
-                    workers_late_ratio,
-                ],
-            ).all(1)
+    group_size = len(datasets)
+    step_size = group_size + 1
+    x_size = len(x_ticks)
 
-            if np.sum(data_selection) == expected_no_of_data_points:
-                boxes.append(data[data_selection, [5]])
-            else:
-                boxes.append([])
-
+    for idx, (series, label, color) in enumerate(zip(datasets, labels, colors)):
         bp = ax.boxplot(
-            boxes,
-            positions=np.array(range(len(workers_per_workplace_counts))) * step_size
-            + workers_late_ratio_idx,
+            series,
+            positions=[i * step_size + idx for i in range(x_size)],
             showfliers=False,
-            widths=0.6,
         )
-        set_box_color(bp, color)
-
-        ax.yaxis.grid(True, linestyle="-", which="major", color="lightgrey", alpha=0.5)
-
-        workers_late_ratio_percentage = int(workers_late_ratio * 100)
-        plt.plot([], c=color, label=f"{workers_late_ratio_percentage}% late workers")
+        setcolor(bp, color)
+        plt.plot([], c=color, label=label)
 
     plt.legend()
-
-    plt.xticks(
-        [
-            x + group_size / 2.0 - 0.5
-            for x in range(0, step_size * len(workers_per_workplace_counts), step_size)
-        ],
-        [int(x) for x in workers_per_workplace_counts],
-    )
-    plt.xlim(-1, step_size * len(workers_per_workplace_counts) - space_size + 1)
-
-    ax.set_xlabel("Workers in a shift (3 shifts in a factory)", fontsize=12)
-    ax.set_ylabel("Computation time (ms)", fontsize=12)
-
-    if measure_phase == 0:
-        measure_phase_desc = "17 minutes before the start of the shift (notification about workers potentially late)"
-    else:
-        measure_phase_desc = (
-            "13 minutes before the start of the shift (selection of standbys)"
-        )
-
-    # ax.set_title(measure_phase_desc)
-
+    plt.xticks([(x * step_size) + group_size / 2 - 0.5 for x in range(x_size)], x_ticks)
+    plt.xlim(-1, step_size * x_size)
     fig.tight_layout()
 
-    plt.savefig(f"figures/{int(measure_phase)}.pdf")
+    return fig, ax
+
+
+def plot_moreprojects():
+    files = ["moreprojects-" + s for s in ("optimizing", "satisfying", "onebyone")]
+    hues = [0, 0.333, 0.666]
+    headers = ["Optimizing solver", "Satisfying solver", "One-by-one solver"]
+    datasets = []
+    labels = []
+    colors = []
+    room_counts = [3, 5]
+
+    for ifn, hue, header in zip(files, hues, headers):
+        data = read_csv(ifn)
+        for i, room_count in enumerate(room_counts):
+            subsel = data[data.lunch_n == room_count]
+            subsel = subsel[~subsel.failed]
+            subsel = subsel[["hungry", "nsec"]]
+            grouping = subsel.groupby("hungry")["nsec"]
+            series = [s for _, s in grouping]
+
+            label = f"{header} - {room_count} rooms"
+            color = colorsys.hsv_to_rgb(hue + 0.15 * i, 0.9, 0.86)
+            datasets.append(series)
+            labels.append(label)
+            colors.append(color)
+
+    x_ticks = range(5, len(datasets[0]) + 5)
+
+    fig, ax = box_graph(datasets, labels, colors, x_ticks)
+    ax.set_ylabel("Computation time (ms)")
+    ax.set_xlabel("Number of workers")
+
+    fig.tight_layout()
+    plt.savefig("moreprojects.pdf")
     plt.close()
 
-    # plt.show()
+
+def plot_morerooms():
+    files = ["morerooms-" + s for s in ("optimizing", "satisfying", "onebyone")]
+    hues = [0, 0.333, 0.666]
+    headers = ["Optimizing solver", "Satisfying solver", "One-by-one solver"]
+    datasets = []
+    labels = []
+    colors = []
+
+    for ifn, hue, header in zip(files, hues, headers):
+        data = read_csv(ifn)
+        subsel = data[~data.failed & (data.hungry <= 25)]
+        subsel = subsel[["hungry", "nsec"]]
+        grouping = subsel.groupby("hungry")["nsec"]
+        series = [s for _, s in grouping]
+
+        color = colorsys.hsv_to_rgb(hue, 0.9, 0.76)
+        datasets.append(series)
+        labels.append(header)
+        colors.append(color)
+
+    maxticks = max(len(d) for d in datasets)
+    x_ticks = range(5, maxticks + 5)
+
+    fig, ax = box_graph(datasets, labels, colors, x_ticks)
+    ax.set_ylabel("Computation time (ms)")
+    ax.set_xlabel("Number of workers")
+
+    fig.tight_layout()
+    plt.savefig("morerooms.pdf")
+    plt.close()
+
+
+
+def main():
+    plot_moreprojects()
+    plot_morerooms()
+
+
+if __name__ == "__main__":
+    main()
