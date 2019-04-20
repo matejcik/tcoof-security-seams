b33e166062644e8502e9857f21de1012df440f6a
matejcik <ja@matejcik.cz>
2019-04-20 18:53:41 +0200

    fix type erasure bug in Notifiable

--- src/main/scala/tcof/Notifiable.scala
+++ src/main/scala/tcof/Notifiable.scala
@@ -1,14 +1,15 @@
 package tcof
 import scala.collection.immutable.Traversable
 import scala.collection.mutable
+import scala.reflect.ClassTag
 
 trait Notification
 
 trait Notifiable {
   private[tcof] val _notificationsReceived = mutable.Set.empty[Notification]
 
-  def notified[T <: Notification]: Boolean =
-    _notificationsReceived.exists(_.isInstanceOf[T])
+  def notified[T <: Notification](implicit tag: ClassTag[T]): Boolean =
+    _notificationsReceived.exists(tag.runtimeClass.isInstance(_))
   def notified(notification: Notification): Boolean =
     _notificationsReceived.contains(notification)
   def notify(notification: Notification): Unit =
